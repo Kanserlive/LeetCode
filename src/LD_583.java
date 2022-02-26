166dee30879c660bbcea19a465801d65bf2e33af
hp <f1026926376@163.com>
2022-02-27 01:12:52 +0800

    2.27 最长公共子序列/最长公共子数组DP

---
+++ src/LD_583.java
@@ -0,0 +1,22 @@
+public class LD_583 {
+    class Solution {
+        public int minDistance(String word1, String word2) {
+            int n=word1.length();
+            int m=word2.length();
+            int [][]dp=new int [n+1][m+1];
+            for(int i=1;i<=n;i++){
+                for(int j=1;j<=m;j++){
+                    if(word1.charAt(i-1)==word2.charAt(j-1)){
+                        dp[i][j]=dp[i-1][j-1]+1;
+                    }else{
+                        dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                    }
+                }
+            }
+            int num=dp[n][m];
+            num=m+n-2*num;
+            return num;
+
+        }
+    }
+}
