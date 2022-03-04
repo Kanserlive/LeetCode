59bb68b3ab2567c9a2cba37ec3285727761f8e23
hp <f1026926376@163.com>
2022-03-05 01:03:56 +0800

    3.5 最长特殊序列

---
+++ src/LD_521.java
@@ -0,0 +1,9 @@
+public class LD_521 {class Solution {
+    public int findLUSlength(String a, String b) {
+        if(a.equals(b)){
+            return -1;
+        }
+        return Math.max(a.length(),b.length());
+    }
+}
+}
