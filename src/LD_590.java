242ac2058fcf1ad8d9afab0d87018488a60cc0e5
hp <f1026926376@163.com>
2022-03-12 14:40:54 +0800

    3.12 n叉树的后序遍历

---
+++ src/LD_590.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class LD_590 {
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+
+    class Solution {
+        List<Integer> ans=new ArrayList<>();
+        public List<Integer> postorder(Node root) {
+            dfs(root);
+            if(root!=null){
+                ans.add(root.val);
+            }
+            return ans;
+        }
+
+        public void dfs(Node root){
+            if(root==null){
+                return;
+            }
+            for(Node num:root.children){
+                dfs(num);
+                ans.add(num.val);
+            }
+        }
+    }
+}
